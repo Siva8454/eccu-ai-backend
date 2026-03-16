2ce796b4ec3954e3a1d4047a9c3244d6300da6e7
Siva Prakash <siva.prakash@eccu.edu>
2026-03-16 17:25:35 +0530

    createindexes.js is added

---
+++ createIndexes.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const { QdrantClient } = require("@qdrant/js-client-rest");
+
+
+const client = new QdrantClient({
+  url: process.env.QDRANT_URL,
+  apiKey: process.env.QDRANT_API_KEY
+});
+
+const COLLECTION = "eccu_knowledge";
+
+async function createIndexes() {
+
+  console.log("Creating payload indexes...");
+
+  await client.createPayloadIndex(COLLECTION, {
+    field_name: "courseId",
+    field_schema: "integer"
+  });
+
+  await client.createPayloadIndex(COLLECTION, {
+    field_name: "moduleNumber",
+    field_schema: "integer"
+  });
+
+  await client.createPayloadIndex(COLLECTION, {
+    field_name: "courseName",
+    field_schema: "keyword"
+  });
+
+  await client.createPayloadIndex(COLLECTION, {
+    field_name: "type",
+    field_schema: "keyword"
+  });
+
+  console.log("Indexes created successfully");
+}
+
+createIndexes();
